52c144e61906310c9b76a690223936cdd76b891c
ramon <ramonruizrc@gmail.com>
2020-05-25 09:00:09 -0500

    clase 1

---
+++ clase1/clase1.js
@@ -0,0 +1,15 @@
+var nombre //Declarar variable
+nombre = 'Juan' //Definir variable
+
+var apellido = 'Ruiz'
+
+var edad = 18
+edad = '18 años'
+
+console.log('Hola ' + nombre + ' ' + apellido);
+console.log('Tengo ' + edad);
+
+
+
+//En algunos casos(la mayoria) el ; es opcional
+//JavaScript es debilmente tipado, lo que quiere decir que 
